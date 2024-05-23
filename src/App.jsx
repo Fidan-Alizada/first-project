7aeb12c5925e99913c42afd64fc6e3bd134af251
Fidan-Alizada <160149958+Fidan-Alizada@users.noreply.github.com>
2024-05-23 23:18:53 +0400

    first-react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './companents/Header/Header'
+import Main from './main'
+import Footer from './companents/Header/Footer'
+ const App = () => {
+  return (
+    <div >
+      <Header />
+      <Main/>
+      <Footer/>
+    </div>
+  )
+}
+export default App
